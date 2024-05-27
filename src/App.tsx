5bd4639c3df23dea0200ccf4dade885115e514a4
Tristan <dumitruerwin@yahoo.com>
2024-05-27 20:19:04 +0300

    Fix link

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Show from './show/Show';
-import Start from './start/Start';
 import './App.scss'
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<Start />} />
-          <Route path="/show" element={<Show />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className='App'>
+      <Show />
+    </div>
   )
 }
 
--- src/start/Start.tsx
+++
@@ -1,9 +0,0 @@
-import './Start.scss';
-
-export default function Start() {
-    return (
-        <div className="start">
-            <h1>Start</h1>
-        </div>
-    );
-}
